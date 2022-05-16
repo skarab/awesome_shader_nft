5f9eb3ddf944dc90d3d8aa5347c7941962b548ae
skarab <a.chambriat@gmail.com>
2022-05-16 18:24:20 +0200

    cleaning

--- Assets/scripts/PearlRenderPipeline.cs
+++ Assets/scripts/PearlRenderPipeline.cs
@@ -10,12 +10,6 @@
 
 	public class PearlRenderPipeline : RenderPipeline
 	{
-		public static class ShaderTags
-		{
-			public static readonly ShaderTagId MainPass = new ShaderTagId("ForwardBase");
-			public static readonly ShaderTagId SRPDefaultUnlit = new ShaderTagId("SRPDefaultUnlit");
-		}
-
 		public Action<ScriptableRenderContext, CommandBuffer, Camera, CullingResults> OnRenderCamera;
 
 		private PearlRenderPipelineAsset _renderPipelineAsset = null;
@@ -65,74 +59,16 @@
 
 				context.SetupCameraProperties(camera);
 
-#if UNITY_EDITOR
-				if (camera.cameraType == CameraType.SceneView && Handles.ShouldRenderGizmos())
-				{
-					ScriptableRenderContext.EmitWorldGeometryForSceneView(camera);
-				}
-#endif
-
-				//_commandBuffer.SetRenderTarget(camera.targetTexture);
-				//_commandBuffer.SetViewport(new Rect(0, 0, camera.pixelWidth, camera.pixelHeight));
-				//_commandBuffer.ClearRenderTarget(true, true, camera.backgroundColor);
-				//context.ExecuteCommandBuffer(_commandBuffer);
-				//_commandBuffer.Clear();
-
 				if (OnRenderCamera != null)
 				{
 					OnRenderCamera(context, _commandBuffer, camera, cullingResults);
 				}
-
-				/*
-				SortingSettings sortingSettings = new SortingSettings(camera);
-				DrawingSettings drawSettings = new DrawingSettings(ShaderTags.MainPass, sortingSettings)
-				{
-					perObjectData = 0
-				};
-				FilteringSettings filterSettings = new FilteringSettings(RenderQueueRange.all);
-
-				sortingSettings.criteria = SortingCriteria.CommonOpaque;
-				drawSettings.sortingSettings = sortingSettings;
-				filterSettings.renderQueueRange = RenderQueueRange.opaque;
-				context.DrawRenderers(cullingResults, ref drawSettings, ref filterSettings);
-
-#if UNITY_EDITOR
-				DrawDefaultObjects(ref context, camera, cullingResults);
-
-				if (Handles.ShouldRenderGizmos())
-				{
-					context.DrawGizmos(camera, GizmoSubset.PreImageEffects);
-				}
-#endif
-				*/
 
 				context.Submit();
 				EndCameraRendering(context, camera);
 			}
 
 			EndFrameRendering(context, cameras);
-		}
-
-		private void DrawDefaultObjects(ref ScriptableRenderContext context, Camera camera, CullingResults cullingResults, int layerMask = -1)
-		{
-			// Setup DrawSettings and FilterSettings
-			SortingSettings sortingSettings = new SortingSettings(camera)
-			{
-				criteria = SortingCriteria.CommonTransparent
-			};
-
-			ShaderTagId defaultPass = ShaderTags.SRPDefaultUnlit;
-			DrawingSettings drawSettings = new DrawingSettings(defaultPass, sortingSettings)
-			{
-				perObjectData = PerObjectData.None
-			};
-			//This will let you draw shader passes without the LightMode,
-			//thus it draws the default UGUI materials
-			drawSettings.SetShaderPassName(1, defaultPass);
-
-			FilteringSettings filterSettings = new FilteringSettings(RenderQueueRange.all, layerMask);
-
-			context.DrawRenderers(cullingResults, ref drawSettings, ref filterSettings);
 		}
 	}
 }
